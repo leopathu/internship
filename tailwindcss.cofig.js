a1bec91f63eb964f66adaa2be041530785ce4c0b
surthick512 <surthick512@gmail.com>
2023-07-21 11:50:11 +0530

    project

---
+++ tailwindcss.cofig.js
@@ -0,0 +1,18 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('background.jpg')",
+        }
+      }
+    }
+  }
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('background2.jpg')",
+        }
+      }
+    }
+  }
